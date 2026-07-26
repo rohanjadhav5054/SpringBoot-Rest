dcce2dff3e34751abb85b64b4d774b7ac3a05822
Rohan <xrohanjadhav5054@gmail.com>
2026-07-26 23:34:00 +0530

    Spring AOP intro doc

--- src/main/java/org/rohan/springbootrest/aop/LoggingAspect.java
+++ src/main/java/org/rohan/springbootrest/aop/LoggingAspect.java
@@ -11,3 +11,18 @@ public class LoggingAspect {
         LOGGER.info("Method Called !");
     }
 }
+/*
+* logies:
+○ Aspect: A module that encapsulates behaviors affecting multiple
+classes.
+○ Join Point: A point in the execution of a program, such as method
+execution or exception handling.
+○ Advice: The action taken by an aspect at a particular join point (e.g.,
+@Before, @After).
+○ Pointcut: The expression that defines at which join points advice
+should be applied.
+○ Target: The object being advised by one or more aspects.
+○ AOP Proxy: A proxy object created by the AOP framework.
+○ Weaving: The process of linking aspects with other application types.
+* */
+
